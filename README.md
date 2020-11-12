ff8e23640675681a03d8e3ecdaa9936b7e663688
Gowtham Babu <babu.g@husky.neu.edu>
2020-11-12 17:39:30 -0500

    added dockerfile, deployment file and helm setup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Multicontainer_Application
-Multicontainer Microservice Application
+# Multicontainer Orchestration
+
+This is a simple order managment application
+
+
+#######Docker Product Push
+docker exec -i <CHANGE_TO_CONTAINER_NAME> python add_products.py
+
+###### Docker compose
+docker-compose -f docker-compose.deploy.yml up -d
+
+##### Docker-machine
+docker-machine create packt-order-management
+
+docker-machine start packt-order-management
+
+docker-machine env packt-order-management
+$ eval $(docker-machine env packt-order-management)
+
+
+docker-machine ip packt-order-management
---
+++ product_service/upload_docker.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+export DOCKER_ID_USER="gwtm11"
+export DOCKER_CONTAINER_NAME="product_container"
+dockerpath = gwtm11/$DOCKER_CONTAINER_NAME
+
+# Step 2:  
+# Authenticate & tag
+
+
+#docker Login
+docker login
+
+# Step 3: 
+#docker build
+docker build --tag=$DOCKER_ID_USER/$DOCKER_CONTAINER_NAME:latest .
+
+
+# docker tag frontend_container $DOCKER_ID_USER/frontend
+
+#docker push
+docker push $DOCKER_ID_USER/$DOCKER_CONTAINER_NAME:latest
+# echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+# docker push gwtm11/frontend_container
